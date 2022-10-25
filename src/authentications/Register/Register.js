e22551dbd4f86cf43609975565f43f20f8bcadd9
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-25 09:16:53 +0600

    complate login and registration form

--- src/authentications/Register/Register.js
+++ src/authentications/Register/Register.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = () => {
     return (
         <div>
-            <h2>Register form</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control name="name" type="text" placeholder="Your Name" />
+                    {/* <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text> */}
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your PhotoURL</Form.Label>
+                    <Form.Control name="photoURL" type="text" placeholder="Enter PhotoURL" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="password" type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
