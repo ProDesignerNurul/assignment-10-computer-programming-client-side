475f451a484ac72edc0b75fd3e8a1f2991b5b8d5
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-25 08:48:15 +0600

    setup routes

---
+++ src/Main/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+
+                    <Col>
+                    <h2>left side navbar</h2>
+                    </Col>
+
+                    <Col>
+                    <Outlet></Outlet>
+                    </Col>
+
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHMrJxZwxzvK8uulmNtU8090jDejb5J_M",
+  authDomain: "computer-programming-d1372.firebaseapp.com",
+  projectId: "computer-programming-d1372",
+  storageBucket: "computer-programming-d1372.appspot.com",
+  messagingSenderId: "30303359924",
+  appId: "1:30303359924:web:beffe2c98f5b042aa558e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
