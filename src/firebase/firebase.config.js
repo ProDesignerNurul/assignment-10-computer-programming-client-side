e2f60883c57c8277188400c249f8f43ddc94d83f
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-26 21:51:31 +0600

    hide firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHMrJxZwxzvK8uulmNtU8090jDejb5J_M",
-  authDomain: "computer-programming-d1372.firebaseapp.com",
-  projectId: "computer-programming-d1372",
-  storageBucket: "computer-programming-d1372.appspot.com",
-  messagingSenderId: "30303359924",
-  appId: "1:30303359924:web:beffe2c98f5b042aa558e1"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
